1dacdb874b999355f74cd37d6433a1771d41f4c8
Carl Trimbach <ctrimbac@cs.brown.edu>
2017-11-07 16:15:18 -0500

    updated data collection

--- run_on_grid.sh
+++ run_on_grid.sh
@@ -18,13 +18,13 @@ do
     python -c "import random_mdp_exp as r; r.exp_file($SIZE, $ACTIONS, $SEED)"
     cd $SEED
     OUTFILE="results.csv"
-    echo -n "train_policy, greedy_policy, err," >> $OUTFILE
+    echo -n "train_policy, greedy_policy, err" >> $OUTFILE
     QS=(q_L q_R)
     for q in ${QS[@]};
     do
         for i in `seq 0 $ITER`;
         do
-            echo -n " $q$i," >> $OUTFILE
+            echo -n ", $q$i" >> $OUTFILE
         done
     done
     WEIGHTS=(w_L w_R)
@@ -32,7 +32,7 @@ do
     do
         for j in `seq 0 $POLYDIM`;
         do
-            echo -n " $w$j," >> $OUTFILE
+            echo -n ", $w$j" >> $OUTFILE
         done
     done
     DS=(d_L d_R)
@@ -40,10 +40,11 @@ do
     do
         for i in `seq 0 $ITER`;
         do
-            echo -n " $d$i," >> $OUTFILE
+            echo -n ", $d$i" >> $OUTFILE
         done
     done
 
+    echo "" >> $OUTFILE
 
     for t in `seq 1 $NUMTRIALS`;
     do
