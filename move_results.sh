33ef4fab084bde8031f2f8c62ba34f6782e62a93
Carl Trimbach <ctrimbac@cs.brown.edu>
2017-11-08 13:47:25 -0500

    move_results updated

--- move_results.sh
+++ move_results.sh
@@ -2,5 +2,17 @@
 
 NOW=$(date +%s)
 
+for d in [0-9]*/;
+do
+	for f in $d/*.o*;
+	do
+		cat $f >> $d/results.csv
+	done
+	for f in $d/*.[o,e]*;
+	do
+		rm $f
+	done
+done
+
 mkdir ./results/$NOW
 mv [0-9]* ./results/$NOW/.
